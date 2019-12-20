4aabdaadfbbfff49c873ebd0037dc1eb68a5d685
Joseph Doiron <josephdoiron1234@yahoo.com>
2019-12-20 06:36:45 -0500

    moved class definition outside graphics

--- projectileMotion.py
+++ projectileMotion.py
@@ -1,9 +1,61 @@
 from graphics import *
+from graphics import _root
+from math import cos, sin, tan
+from math import radians
+import numpy as np
+from graphics import *
 import time
 
+_gravity = 9.81
+
+
+class Projectile(Circle):
+
+    def __init__(self, v, theta, pos_x=0.0, pos_y=0.0):
+        self.theta = theta
+        self.v = v
+        self.v_x = v * cos(radians(theta))
+        self.v_y = v * sin(radians(theta))
+        self.time_of_flight = (2 * v * sin(radians(theta))) / _gravity
+        self.range = ((v**2)*(sin(2*radians(theta)))) / _gravity
+        self.max_height = ((v**2)*sin(radians(theta))**2) / 2*_gravity
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        Circle.__init__(self, Point(self.pos_x, self.pos_y), 2)
+
+    def __str__(self):
+        return "Projectile instance located at ({self.pos_x},{self.pos_y})".format(self=self)
+
+# NEED TO FIX THIS PART
+    def move(self, t):
+        canvas = self.canvas
+        if canvas and not canvas.isClosed():
+            trans = canvas.trans
+            self.pos_x = self.v_x * t
+
+            if self.pos_x > self.range:
+                self.pos_x = self.range / trans.xscale
+                self.pos_y = 0.0
+            else:
+                self.pos_y = (tan(radians(self.theta))*self.pos_x) - \
+                     (_gravity / (2*(self.v**2)*(cos(radians(self.theta))**2))*(self.pos_x**2)) / trans.yscale
 
 
 
+    def move(self, t):
+        self.update_position(t)
+        canvas = self.canvas
+        if canvas and not canvas.isClosed():
+            trans = canvas.trans
+            if trans:
+                x = self.pos_x
+                y = -self.pos_y
+            else:
+                x = self.pos_x
+                y = self.pos_y
+            self.canvas.move(self.id, x, y)
+            if canvas.autoflush:
+                _root.update()
 
 
 def main(velocity, angle):
