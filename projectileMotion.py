6ce303477a8c982d9096f90a08b012a46826de11
Doiron, Joseph <jdoiron2@unum.com>
2019-12-26 00:29:39 -0500

    added class Projectile as an extension to graphics.py

--- projectileMotion.py
+++ projectileMotion.py
@@ -1,6 +1,6 @@
 from graphics import *
 from graphics import _root
-from math import cos, sin, tan
+from math import cos, sin, tan, sqrt, pow
 from math import radians
 import numpy as np
 from graphics import *
@@ -9,73 +9,70 @@ import time
 _gravity = 9.81
 
 
-class Projectile(Circle):
+class Projectile(Point):
 
-    def __init__(self, v, theta, pos_x=0.0, pos_y=0.0):
-        self.theta = theta
+    def __init__(self, v, theta, x=0.0, y=0.0):
+        self.initial_v = v
         self.v = v
-        self.v_x = v * cos(radians(theta))
-        self.v_y = v * sin(radians(theta))
-        self.time_of_flight = (2 * v * sin(radians(theta))) / _gravity
-        self.range = ((v**2)*(sin(2*radians(theta)))) / _gravity
-        self.max_height = ((v**2)*sin(radians(theta))**2) / 2*_gravity
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        Circle.__init__(self, Point(self.pos_x, self.pos_y), 2)
+        self.v_x = self.v * cos(radians(theta)) # constant
+        self.v_y = self.v * sin(radians(theta)) # changes with time
+        self.theta = theta
+        self.x = x
+        self.y = y
+        self.max_height = (pow(self.initial_v,2) * pow(sin(radians(theta)),2))/(2*_gravity)
+        self.range = (pow(self.initial_v,2) * (sin(radians(2*theta)))) / _gravity
+        Point.__init__(self, self.x, self.y)
 
     def __str__(self):
-        return "Projectile instance located at ({self.pos_x},{self.pos_y})".format(self=self)
+        return "Projectile instance located at ({self.x},{self.pos_y})".format(self=self)
 
-# NEED TO FIX THIS PART
-    def move(self, t):
-        canvas = self.canvas
-        if canvas and not canvas.isClosed():
-            trans = canvas.trans
-            self.pos_x = self.v_x * t
+    def update_velocity(self, time):
+        self.v_y = self.v_y - (_gravity * time)
+        self.v = sqrt(pow(self.v_x,2)+pow(self.v_y,2))
 
-            if self.pos_x > self.range:
-                self.pos_x = self.range / trans.xscale
-                self.pos_y = 0.0
-            else:
-                self.pos_y = (tan(radians(self.theta))*self.pos_x) - \
-                     (_gravity / (2*(self.v**2)*(cos(radians(self.theta))**2))*(self.pos_x**2)) / trans.yscale
+    def _update_position(self, time):
+        self.x = self.initial_v * time * cos(radians(self.theta))
+        self.y = (self.initial_v * time * sin(radians(self.theta))) - (.5 * _gravity * pow(time,2))
 
+    def move(self, time, dt):
 
+        self._update_position(time-dt)
+        x_before = self.x
+        y_before = self.y
+        self._update_position(time)
 
-    def move(self, t):
-        self.update_position(t)
+        dx = self.x - x_before
+        dy = self.y - y_before
+
         canvas = self.canvas
         if canvas and not canvas.isClosed():
             trans = canvas.trans
             if trans:
-                x = self.pos_x
-                y = -self.pos_y
+                x = dx/ trans.xscale 
+                y = -dy / trans.yscale
             else:
-                x = self.pos_x
-                y = self.pos_y
+                x = dx
+                y = dy
             self.canvas.move(self.id, x, y)
             if canvas.autoflush:
                 _root.update()
+                
 
 
-def main(velocity, angle):
-    cir1 = Projectile(100, 20)
-    cir1.setFill("black")
-    win = GraphWin('Back and Forth', 1000, 1000)
-    win.setCoords(0, 0, cir1.range, cir1.max_height)
+def main(v, angle):
+    p = Projectile(v, angle)
+    win = GraphWin('Projectile', 900, 400)
+    win.setCoords(0, 0, p.range+20, p.max_height+20)
 
-    cir1.draw(win)
+    p.draw(win)
 
-    delta_t = 0.0
-    while cir1.getCenter().getX() <= cir1.range:
-        delta_t += .1
-        cir1.move(delta_t)
-        time.sleep(1)
+    t = 0.0
+    dt = .1
+    while p.x <= p.range:
+        p.move(t, dt)
+        t += dt
+        time.sleep(.02)
 
-    win.getMouse()
     win.close()
 
-
-
-
-main(100,50)
+main(200,40)
